ace589567e14f8a02f4e3ce1f042c994fe12318e
Kevin Jiang <kzj1995@yahoo.com>
2017-11-11 16:34:31 -0500

    Fixes to web scraper

--- src/parse.py
+++ src/parse.py
@@ -4,49 +4,69 @@ import re
 
 
 def parse_bbc_article(url):
-    page = urllib2.urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
-    story = soup.find('div', attrs={'class': 'story-body'})
+    try:
+        print("URL:{}".format(url))
+        page = urllib2.urlopen(url)
+        soup = BeautifulSoup(page, 'html.parser')
+        story = soup.find('div', attrs={'class': 'story-body'})
 
-    title = story.find('h1').text
-    date = story.find('div', attrs={'class': 'date'}).text
+        title = story.find('h1').text
+        date = story.find('div', attrs={'class': 'date'}).text
 
-    text = [x.text for x in story.find('div', attrs={'class': 'story-body__inner'}).findAll("p")]
+        text = [x.text for x in story.find('div', attrs={'class': 'story-body__inner'}).findAll("p")]
 
-    links = []
-    for l in [x['href'] for x in soup.find_all('a', href=True)]:
-        m = re.search(r'\d{8}$', l)
-        if m and "mailto" not in l and "twitter" not in l and "account" not in l:
-            links.append(l)
+        links = []
+        for l in [x['href'] for x in soup.find_all('a', href=True)]:
+            m = re.search(r'\d{8}$', l)
+            if m and "mailto" not in l and "twitter" not in l and "account" not in l:
+                links.append(l)
 
-    return {
-        'title': title,
-        'author': "BBC",
-        'date': date,
-        'text': text,
-        'links': links
-    }
+        return {
+            'title': title,
+            'author': "BBC",
+            'date': date,
+            'text': text,
+            'links': links
+        }
+    except Exception:
+        return {
+            'title': "",
+            'author': "",
+            'date': "",
+            'text': "",
+            'links': ""
+        }
 
 def parse_abc_article(url):
-    page = urllib2.urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
+    try:
+        print("URL:{}".format(url))
+        page = urllib2.urlopen(url)
+        soup = BeautifulSoup(page, 'html.parser')
 
-    title = soup.find('header', attrs={'class': 'article-header'}).find('h1').text
-    date = soup.find('span', attrs={'class': 'timestamp'}).text
-    author = ", ".join([x.text.replace("\n", "").replace("By ", "").title() for x in soup.findAll('div', attrs={'rel': 'author'})])
+        title = soup.find('header', attrs={'class': 'article-header'}).find('h1').text
+        date = soup.find('span', attrs={'class': 'timestamp'}).text
+        author = ", ".join([x.text.replace("\n", "").replace("By ", "").title() for x in soup.findAll('div', attrs={'rel': 'author'})])
 
-    text = " ".join([x.text for x in soup.findAll(itemprop="articleBody")])
+        text = " ".join([x.text for x in soup.findAll(itemprop="articleBody")])
 
-    links = [x['href'] for x in soup.find('section', attrs={'id': 'news-feed'}).find_all('a', href=True)]
+        links = [x['href'] for x in soup.find('section', attrs={'id': 'news-feed'}).find_all('a', href=True)]
 
-    return {
-        'title': title,
-        'author': author,
-        'date': date,
-        'text': text,
-        'links': links
-    }
+        return {
+            'title': title,
+            'author': author,
+            'date': date,
+            'text': text,
+            'links': links
+        }
+    except Exception:
+        return {
+            'title': "",
+            'author': "",
+            'date': "",
+            'text': "",
+            'links': ""
+        }
 
 # parse_bbc_article("http://www.bbc.com/news/world-europe-41798254")
 # parse_bbc_article("http://www.bbc.com/news/technology-41942306")
-parse_abc_article("http://abcnews.go.com/Politics/roy-moore-promises-revelations-motivations-sexual-misconduct-allegations/story?id=51083664")
+# parse_abc_article("http://abcnews.go.com/Politics/roy-moore-promises-revelations-motivations-sexual-misconduct-allegations/story?id=51083664")
--- src/web_scraper.py
+++ src/web_scraper.py
@@ -8,11 +8,11 @@ class WebScraper:
 
     def __init__(self):
         self.frontier_urls = []
-        self.explored_url_set = {}
+        self.seen_url_set = set()
         self.all_scraped_articles = []
 
     def process_article(self, parsed_article, article_hostname):
-        if (parsed_article.get("title") != "") and (parsed_article.get("date") != "") \
+        if (parsed_article.get("title") != "") and (parsed_article.get("date") != "") and \
         (parsed_article.get("text") != ""):
             self.all_scraped_articles.append({
                 "title": parsed_article.get("title"),
@@ -20,26 +20,44 @@ class WebScraper:
                 "text": parsed_article.get("text"),
                 "author": parsed_article.get("author")
             })
+            print("GOT ARTICLE {}".format(len(self.all_scraped_articles)))
         for article_url in parsed_article["links"]:
-            current_article = urlparse(article_url):
+            current_article = urlparse(article_url)
+            #print("ARTICLE URL:{}".format(article_url))
             if (current_article.netloc == ("www." + article_hostname)) or \
-            (current_article.netloc == article_hostname) or (current_article.netloc = ""):
-               current_article_host = "www." + article_hostname
-                final_article_url = current_article_host + current_article.path + current_article.query
-                if final_article_url not in self.explored_url_set:
+            (current_article.netloc == article_hostname) or (current_article.netloc == ""):
+                current_article_host = "http://www." + article_hostname
+                final_article_url = current_article_host + current_article.path + "?" + \
+                current_article.query
+                #print("FINAL ARTICLE URL:{}".format(final_article_url))
+                if final_article_url not in self.seen_url_set:
+                    self.seen_url_set.add(final_article_url)
                     self.frontier_urls.append(final_article_url)
 
-    def scrape_articles(seed_urls):
+    def scrape_articles(self, seed_urls):
         self.frontier_urls = seed_urls
-        while len(self.frontier_urls) > 0 and (len(self.all_scraped_articles) <= 100):
+        a = 0
+        while len(self.frontier_urls) > 0 and (len(self.all_scraped_articles) <= 4000):
             current_article = self.frontier_urls.pop(0)
-            self.explored_url_set.add(current_article)
             parse_uri = urlparse(current_article)
+            #print("HOSTY: {}".format(parse_uri.netloc))
             if parse_uri.netloc == "www.bbc.com" or parse_uri.netloc == "bbc.com":
-                parsed_article = parse_bbc_article(parse_uri)
+                #print("HELLO")
+                parsed_article = parse_bbc_article(current_article)
                 self.process_article(parsed_article, "bbc.com")
             elif parse_uri.netloc == "www.abcnews.go.com" or parse_uri.netloc == "abcnews.go.com":
-                parsed_article = parse_abc_article(parse_uri)
+                #print("HELLO")
+                parsed_article = parse_abc_article(current_article)
                 self.process_article(parsed_article, "abcnews.go.com")
+            #print("FRONTIER:{}".format(self.frontier_urls))
+            a += 1
+        #print("ITERATIONS: {}".format(a))
+        print("ENDING FRONTIER SIZE: {}".format(len(self.frontier_urls)))
         with open("data/articles_{}".format(len(self.all_scraped_articles)), "w") as f:
             json.dump(self.all_scraped_articles, f)
+
+if __name__ == '__main__':
+    web_scraper = WebScraper()
+    web_scraper.scrape_articles(["http://www.bbc.com/news/world-us-canada-41954436", \
+        "http://www.bbc.com/news/technology-41942310", \
+        "http://www.bbc.com/sport/rugby-union/41934487"])
