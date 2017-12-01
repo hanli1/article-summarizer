34c062db0b28707f314f2c2c79aa2e58ed4f1c17
Kevin Jiang <kzj1995@yahoo.com>
2017-11-30 21:50:52 -0500

    Implement tabbing between sections

--- website/static/main.js
+++ website/static/main.js
@@ -4,6 +4,9 @@ $( document ).ready(function() {
   var pageFetchInProgress = false; //Whether or not a page of articles is currently being fetched
   var currentDate = ""; //The current date of the articles
   var textQuery = ""; //The text query entered in the search box
+  var hideArticleList = false; //Whether or not the article list is hidden
+  var hideDynamicInput = true; //Whether or not the dynamic input page is hidden
+  var hideAbout = true; //Whether or not the about page is hidden
 
 
   $("#search-button").click(send_search_query);
@@ -36,6 +39,29 @@ $( document ).ready(function() {
 
   $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
     var target = $(e.target).attr("href"); // activated tab
+    console.log(target);
+    if (target == "#articles") {
+      hideArticleList = false;
+      hideDynamicInput = true;
+      hideAbout = true;
+      $("#article-results").show();
+      $("#dynamic").hide();
+      $("#about").hide();
+    } else if (target == "#dynamic") {
+      hideArticleList = true;
+      hideDynamicInput = false;
+      hideAbout = true;
+      $("#article-results").hide();
+      $("#dynamic").show();
+      $("#about").hide();
+    } else if (target == "#about") {
+      hideArticleList = true;
+      hideDynamicInput = true;
+      hideAbout = false;
+      $("#article-results").hide();
+      $("#dynamic").hide();
+      $("#about").show();
+    }
   });
 
   function fetchArticleList(textQuery) {
@@ -87,13 +113,15 @@ $( document ).ready(function() {
   }
 
   $(window).scroll(function(){
+    if (!hideArticleList) {
       var windowScroll = $(this).scrollTop();
       var articleWrappers = $('.article-wrapper');
       var homeWrappersHeight = (articleWrappers.length - 1) * articleWrappers.first().height();
-      if  (((homeWrappersHeight - windowScroll) <= 400) && !lastPageFetched && !pageFetchInProgress) {
-         pageFetchInProgress = true;
-         fetchArticleList(textQuery);
+      if (((homeWrappersHeight - windowScroll) <= 400) && !lastPageFetched && !pageFetchInProgress) {
+        pageFetchInProgress = true;
+        fetchArticleList(textQuery);
       }
+    }
   });
 
   $('#article-results').on('change', 'input[name="summary-radio-button"]', function() {
